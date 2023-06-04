7465013728feb235d9ad6a14db6f54a3c6838bb1
erke31-2 <KaungSet>
2023-06-04 20:44:39 +0630

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const App = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <main className="max-w-[375px] mx-auto grid grid-cols-1 lg:grid-cols-5 lg:min-w-full min-h-screen relative font-leagueSpartan">
+    <main className="max-w-[375px] mx-auto grid grid-cols-1 lg:grid-cols-12 lg:min-w-full min-h-screen relative font-leagueSpartan">
       {isOpen && <div className="absolute inset-0 z-20 bg-VeryDarkGray/80 scroll-smooth"></div>}
       <TopComponent isOpen={isOpen} setIsOpen={setIsOpen} />
       <BotComponent />
